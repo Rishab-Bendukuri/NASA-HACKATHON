91e872edeeb1b684f23f8c019ac6b066bba421c0
Rishab-Bendukuri <20071a0509@vnrvjiet.in>
2022-09-23 17:08:17 +0530

    index-clean

--- frontend/notus-react-main/src/views/Index.js
+++ frontend/notus-react-main/src/views/Index.js
@@ -9,16 +9,16 @@ import { SliderData } from "components/Slider/SliderData";
 import './Index-marquee.css'
 export default function Index() {
   return (
-    <>
+    <div className="bg-img">
       <IndexNavbar fixed />
 
       <div className="container row pt-32 ">
         <div className="col-sm-8">
           <div className="pt-32 sm:pt-0">
-              <h2 className="font-semibold text-4xl text-blueGray-600">
+              <h2 className="font-semibold text-4xl text-white">
                 What our website is about?
               </h2>
-              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+              <p className="mt-4 text-lg leading-relaxed text-white">
                  It provides entire information about meteoroids and its co-ordinates. It can be used to track their locations. It also provides us with the information on the pattern of the meteoroids falling towards earth. It also warns when a dangerous asteroid is on a collision course towards earth. It is also helpful for various researchers to gain more insight on the meteoroids such as their size, location and its properties.
               </p>
               
@@ -37,6 +37,6 @@ export default function Index() {
     
       
       <Footer />
-    </>
+    </div>
   );
 }
