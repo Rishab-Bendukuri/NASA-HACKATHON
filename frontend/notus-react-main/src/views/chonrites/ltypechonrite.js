2b41f8b52527029743b5b87df4b562df6ada4d79
Rishab-Bendukuri <20071a0509@vnrvjiet.in>
2022-09-24 07:08:34 +0530

    Update ltypechonrite.js

--- frontend/notus-react-main/src/views/chonrites/ltypechonrite.js
+++ frontend/notus-react-main/src/views/chonrites/ltypechonrite.js
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import CardLineChart from 'components/Cards/CardLineChart'
 
 function ltypechonrite() {
   return (
     <div className='h100 bgc'>
-        <img src="https://live.staticflickr.com/65535/48165861062_644a9c70fa_b.jpg" className="customcrd w-25" alt="..."/>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZVcVotbfR0oFYfTXRWBN4UAtPBPvYa6uOsg&usqp=CAU" className="customcrd w-25" alt="..."/>
         <div>
             <h2 className='text-white ps-3'>The L type ordinary chondrites are the second most common group of meteorites, accounting for approximately 35% of all those catalogued, and 40% of the ordinary chondrites.[1] The ordinary chondrites are thought to have originated from three parent asteroids, with the fragments making up the H chondrite, L chondrite and LL chondrite groups respectively.</h2>
         </div>
+    {/* <CardLineChart/> */}
     </div>
   )
 }
